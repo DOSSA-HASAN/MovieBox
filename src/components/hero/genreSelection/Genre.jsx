82605646b90f8057d17ce53be62363b953ccbe4c
DOSSA-HASAN <hassandossa5@gmail.com>
2025-01-28 20:48:34 +0300

    added tv shows genre selection

--- src/components/hero/genreSelection/Genre.jsx
+++ src/components/hero/genreSelection/Genre.jsx
@@ -4,67 +4,96 @@ import Swal from "sweetalert2"
 
 function Genre() {
 
-    const mappingGenres = [
-        { id: 1, name: "Action" },
-        { id: 2, name: "Adventure" },
-        { id: 3, name: "Animation" },
-        { id: 4, name: "Biography" },
-        { id: 5, name: "Comedy" },
-        { id: 6, name: "Crime" },
-        { id: 7, name: "Documentary" },
-        { id: 8, name: "Drama" },
-        { id: 9, name: "Family" },
-        { id: 10, name: "Fantasy" },
-        { id: 11, name: "History" },
-        { id: 12, name: "Horror" },
-        { id: 13, name: "Music" },
-        { id: 14, name: "Musical" },
-        { id: 15, name: "Mystery" },
-        { id: 16, name: "Romance" },
-        { id: 17, name: "Science Fiction" },
-        { id: 18, name: "Sports" },
-        { id: 19, name: "Superhero" },
-        { id: 20, name: "Thriller" },
+    const movieMappingGenres = [
+        { id: 28, name: "Action" },
+        { id: 12, name: "Adventure" },
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 14, name: "Fantasy" },
+        { id: 36, name: "History" },
+        { id: 27, name: "Horror" },
+        { id: 10402, name: "Music" },
+        { id: 9648, name: "Mystery" },
+        { id: 10749, name: "Romance" },
+        { id: 878, name: "Science Fiction" },
         { id: 21, name: "War" },
-        { id: 22, name: "Western" }
+        { id: 37, name: "Western" }
+    ];
+
+    const tvMappingGenres = [
+        { id: 10759, name: "Action & Adventure" },
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 10762, name: "Kids" },
+        { id: 9648, name: "Mystery" },
+        { id: 10763, name: "News" },
+        { id: 10764, name: "Reality" },
+        { id: 10765, name: "Sci-Fi & Fantasy" },
+        { id: 10766, name: "Soap" },
+        { id: 10767, name: "Talk" },
+        { id: 10768, name: "War & Politics" },
+        { id: 37, name: "Western" }
     ];
 
     const [movieGenres, setMovieGenres] = useState([
-        { id: 1, name: "Action" },
-        { id: 2, name: "Adventure" },
-        { id: 3, name: "Animation" },
-        { id: 4, name: "Biography" },
-        { id: 5, name: "Comedy" },
-        { id: 6, name: "Crime" },
-        { id: 7, name: "Documentary" },
-        { id: 8, name: "Drama" },
-        { id: 9, name: "Family" },
-        { id: 10, name: "Fantasy" },
-        { id: 11, name: "History" },
-        { id: 12, name: "Horror" },
-        { id: 13, name: "Music" },
-        { id: 14, name: "Musical" },
-        { id: 15, name: "Mystery" },
-        { id: 16, name: "Romance" },
-        { id: 17, name: "Science Fiction" },
-        { id: 18, name: "Sports" },
-        { id: 19, name: "Superhero" },
-        { id: 20, name: "Thriller" },
+        { id: 28, name: "Action" },
+        { id: 12, name: "Adventure" },
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 14, name: "Fantasy" },
+        { id: 36, name: "History" },
+        { id: 27, name: "Horror" },
+        { id: 10402, name: "Music" },
+        { id: 9648, name: "Mystery" },
+        { id: 10749, name: "Romance" },
+        { id: 878, name: "Science Fiction" },
         { id: 21, name: "War" },
-        { id: 22, name: "Western" }
+        { id: 37, name: "Western" }
     ]);
 
-    const [selectedGenres, setSelectedGenres] = useState([])
+    const [tvGenres, setTvGenres] = useState([
+        { id: 10759, name: "Action & Adventure" },
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 10762, name: "Kids" },
+        { id: 9648, name: "Mystery" },
+        { id: 10763, name: "News" },
+        { id: 10764, name: "Reality" },
+        { id: 10765, name: "Sci-Fi & Fantasy" },
+        { id: 10766, name: "Soap" },
+        { id: 10767, name: "Talk" },
+        { id: 10768, name: "War & Politics" },
+        { id: 37, name: "Western" }
+    ]);
 
-    const handleAddGenre = (genreId) => {
-        const isDuplicate = selectedGenres.filter(genre => genre.id == genreId)
+    const [selectedMovieGenres, setSelectedMovieGenres] = useState([])
+    const [selectedTvGenres, setSelectedTvGenres] = useState([])
+
+    const handleAddMovieGenre = (genreId) => {
+        const isDuplicate = selectedMovieGenres.filter(genre => genre.id == genreId)
 
         //check if no duplicate found
         if (isDuplicate.length == 0) {
-            if (selectedGenres.length < 3) {
-                //add to selectedGenres 
+            if (selectedMovieGenres.length < 3) {
+                //add to selectedMovieGenres 
                 const addToSelected = movieGenres.filter(movie => (movie.id == genreId))
-                setSelectedGenres(prev => [...prev, ...addToSelected])
+                setSelectedMovieGenres(prev => [...prev, ...addToSelected])
                 //remove from movieGenres
                 const filteredMoviesGenre = movieGenres.filter(movie => (movie.id !== genreId))
                 setMovieGenres(filteredMoviesGenre)
@@ -72,7 +101,7 @@ function Genre() {
             else {
                 Swal.fire({
                     // title: 'Responsive Alert!',
-                    text: 'Max limit reached, can not add more to selection',
+                    text: 'Max limit reached, can not select more movie genres',
                     icon: 'warning',
                     confirmButtonText: 'Okay',
                 })
@@ -80,31 +109,78 @@ function Genre() {
         }
         else {
             // if a duplicate was found
-            //remove it from the selectedGenres
-            const filteredSelected = selectedGenres.filter(genre => genre.id !== genreId)
-            setSelectedGenres(filteredSelected)
+            //remove it from the selectedMovieGenres
+            const filteredSelected = selectedMovieGenres.filter(genre => genre.id !== genreId)
+            setSelectedMovieGenres(filteredSelected)
             //add it to moviesGenre
-            const addToMovies = selectedGenres.filter(genre => genre.id == genreId)
+            const addToMovies = selectedMovieGenres.filter(genre => genre.id == genreId)
             setMovieGenres(prev => [...prev, ...addToMovies])
         }
     }
 
-    console.log("movie genre", movieGenres)
-    console.log("selected genres", selectedGenres)
+    const handleAddTvGenre = (genreId) => {
+        const isDuplicate = selectedTvGenres.filter(genre => genre.id == genreId)
+
+        //check if no duplicate found
+        if (isDuplicate.length == 0) {
+            if (selectedTvGenres.length < 3) {
+                //add to selectedTvGenres 
+                const addToSelected = tvGenres.filter(movie => (movie.id == genreId))
+                setSelectedTvGenres(prev => [...prev, ...addToSelected])
+                //remove from tvGenres
+                const filteredTvGenre = tvGenres.filter(movie => (movie.id !== genreId))
+                setTvGenres(filteredTvGenre)
+            }
+            else {
+                Swal.fire({
+                    // title: 'Responsive Alert!',
+                    text: 'Max limit reached, can not select more tv show genres',
+                    icon: 'warning',
+                    confirmButtonText: 'Okay',
+                })
+            }
+        }
+        else {
+            // if a duplicate was found
+            //remove it from the selectedTvGenres
+            const filteredSelected = selectedTvGenres.filter(genre => genre.id !== genreId)
+            setSelectedTvGenres(filteredSelected)
+            //add it to tvGenre
+            const addToTv = selectedTvGenres.filter(genre => genre.id == genreId)
+            setTvGenres(prev => [...prev, ...addToTv])
+        }
+    }
 
 
     return (
         <section className='genre-section'>
-            <h1>Select Your Genres</h1>
             <main className="genres-selection-cont">
-                {
-                    mappingGenres.map((genre, index) => (
-                        <div onClick={() => handleAddGenre(genre.id)} key={index}
-                            className={selectedGenres.some((selected) => selected.id == genre.id) ? "selected genre" : "genre"}>
-                            <p>{genre.name}</p>
-                        </div>
-                    ))
-                }
+                <article className="movie-genre">
+                    <h1>Select Your Movie Genres</h1>
+                    <div className="genres">
+                        {
+                            movieMappingGenres.map((genre, index) => (
+                                <div onClick={() => handleAddMovieGenre(genre.id)} key={index}
+                                    className={selectedMovieGenres.some((selected) => selected.id == genre.id) ? "selected genre" : "genre"}>
+                                    <p>{genre.name}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </article>
+                <article className="tv-shows-genre">
+                    <h1>Select Your Movie Genres</h1>
+                    <div className="genres">
+                        {
+                            tvMappingGenres.map((genre, index) => (
+                                <div onClick={() => handleAddTvGenre(genre.id)} key={index}
+                                    className={selectedTvGenres.some((selected) => selected.id == genre.id) ? "selected genre" : "genre"}>
+                                    <p>{genre.name}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </article>
             </main>
             <div className="overlay"></div>
         </section >
