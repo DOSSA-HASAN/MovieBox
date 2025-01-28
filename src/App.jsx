82165f38b7ced9434579a3ffd7e0011587b252d3
DOSSA-HASAN <hassandossa5@gmail.com>
2025-01-28 21:37:50 +0300

    fixed routing and established global state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { BrowserRouter, Routes } from "react-router-dom"
-import Navbar from "./components/navbar/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Hero from "./components/hero/Hero"
 import Genre from "./components/hero/genreSelection/Genre"
+import { MovieContext, mContext } from './MovieContext'
+import { useContext } from "react"
+
 
 function App() {
+  return (
+    <>
+      <MovieContext>
+        <BrowserRouter>
+          { }
+          <Routes>
+            <Route path="/" element={<InnerComponent />} />
+          </Routes>
+        </BrowserRouter>
+      </MovieContext>
+    </>
+  )
+}
+
+function InnerComponent() {
+
+  const { setHasSelected, hasSelected } = useContext(mContext)
 
   return (
     <>
-      <BrowserRouter>
-        <Hero />
-        <Genre />
-        <Routes>
-        </Routes>
-      </BrowserRouter>
+      {
+      !hasSelected ? <Genre /> : <Hero />
+      }
     </>
   )
 }
